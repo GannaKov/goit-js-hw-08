2a78cec2766371819b45694c438d8ace3764f4a9
GannaKov <zlatta2000@gmail.com>
2022-10-01 22:06:21 +0300

    Vimeo, currentTime

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,28 @@
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
+player.on('timeupdate', function (e) {
+  //   console.log(e.seconds);
+  //   console.log('played the video!');
+  localStorage.setItem('time', JSON.stringify(e.seconds));
+});
+
+// console.log('a', JSON.parse(localStorage.getItem('time')));
+
+player
+  .setCurrentTime(JSON.parse(localStorage.getItem('time')))
+  .then(function (seconds) {
+    // seconds = the actual time that the player seeked to
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        // the time was less than 0 or greater than the video’s duration
+        break;
+
+      default:
+        // some other error occurred
+        break;
+    }
+  });
