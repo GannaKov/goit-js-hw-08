e4ef5a42d48d856d84ae4818a22b8863ba89ebef
GannaKov <zlatta2000@gmail.com>
2022-10-02 00:06:01 +0300

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,28 +1,30 @@
+import throttle from 'lodash.throttle';
 import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
 
-player.on('timeupdate', function (e) {
-  //   console.log(e.seconds);
-  //   console.log('played the video!');
-  localStorage.setItem('time', JSON.stringify(e.seconds));
-});
-
+player.on(
+  'timeupdate',
+  throttle(function (e) {
+    localStorage.setItem('time', JSON.stringify(e.seconds));
+  }, 1000)
+);
+// ----- Second Var
+// const onPlay = function (e) {
+//   localStorage.setItem('time', JSON.stringify(e.seconds));
+// };
+// player.on('timeupdate', throttle(onPlay, 1000));
 // console.log('a', JSON.parse(localStorage.getItem('time')));
 
 player
   .setCurrentTime(JSON.parse(localStorage.getItem('time')))
-  .then(function (seconds) {
-    // seconds = the actual time that the player seeked to
-  })
+  .then(function (seconds) {})
   .catch(function (error) {
     switch (error.name) {
       case 'RangeError':
-        // the time was less than 0 or greater than the video’s duration
         break;
 
       default:
-        // some other error occurred
         break;
     }
   });
