3f46ef81229878e4198395c4bf867c1af599444d
GannaKov <zlatta2000@gmail.com>
2022-10-02 15:20:10 +0300

    Task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,7 @@
 import throttle from 'lodash.throttle';
 const STORAGE_KEY = 'feedback-form-state';
 const formData = {};
-const formDataStorage = {};
+
 const refs = {
   form: document.querySelector('.feedback-form'),
   textarea: document.querySelector('.feedback-form textarea'),
@@ -11,28 +11,46 @@ refs.form.addEventListener('input', throttle(onFormInput, 500));
 populateForm();
 refs.form.addEventListener('submit', onFormSubmit);
 function onFormInput(evt) {
-  formDataStorage[evt.target.name] = evt.target.value;
+  //Я так и не придумала как это использовать, чтобы правильно перезаписывалось
+  //   formDataStorage[evt.target.name] = evt.target.value;
+  const formDataStorage = {
+    email: refs.form.elements.email.value,
+    message: refs.form.elements.message.value,
+  };
+
   localStorage.setItem(STORAGE_KEY, JSON.stringify(formDataStorage));
 }
 
 function populateForm() {
   try {
     const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
-    if (savedData.message) {
-      refs.textarea.value = savedData.message;
-    }
-    if (savedData.email) {
-      refs.email.value = savedData.email;
-    }
-  } catch {}
+    refs.textarea.value = savedData.message;
+    refs.email.value = savedData.email;
+  } catch {
+    console.dir('Data has been deleted');
+    refs.textarea.value = '';
+    refs.email.value = '';
+  }
 }
 
 function onFormSubmit(evt) {
   evt.preventDefault();
 
-  formData.email = evt.target.elements.email.value;
-  formData.message = evt.target.elements.message.value;
-  console.log(formData); //это не убирать
-  evt.currentTarget.reset();
-  localStorage.removeItem(STORAGE_KEY);
+  //   evt.target.elements.email.value === ''
+  //     ? alert('Please fill email field!')
+  //     : (formData.email = evt.target.elements.email.value);
+  //   evt.target.elements.message.value === ''
+  //     ? alert('Please fill message field!')
+  //     : (formData.message = evt.target.elements.message.value);
+  if (evt.target.elements.email.value === '') {
+    alert('Please fill email field!');
+  } else if (evt.target.elements.message.value === '') {
+    alert('Please fill message field!');
+  } else {
+    formData.email = evt.target.elements.email.value;
+    formData.message = evt.target.elements.message.value;
+    console.log(formData); //это не убирать
+    evt.currentTarget.reset();
+    localStorage.removeItem(STORAGE_KEY);
+  }
 }
